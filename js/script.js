c5c7f69318bda12153d31afac06f8b28755a2d8d
Raphiadev <raviachmadfadila@gmail.com>
2024-01-18 22:11:45 +0800

    test

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-function luas(){
-    var s1 = document.getElementById('s1').value;
-    var s2 = document.getElementById('s1').value;
-    var s3 = document.getElementById('s1').value;
-   
-    var luas =s2*s3;
-
-   document.getElementById('luas').innerHTML = luas;
+function square( )
+{
+var side,area;
+side = parseInt(document.getElementById("first").value);
+area = side*side;
+document.getElementById("num").innerHTML= side + " x " + side
+document.getElementById("num").innerHTML="Luas Persegi adalah : "+area;
 }
+
+function keliling( )
+{
+var sisi,perimeter;
+sisi = parseInt(document.getElementById("second").value);
+perimeter = 4*sisi;
+document.getElementById("num1").innerHTML="Keliling Persegi adalah : "+perimeter;
+}
+
+function fun1() {
+    const first = document.querySelector('#first')
+    first.value = null;
+  }
+
+  function fun2() {
+    const first = document.querySelector('#second')
+    first.value = null;
+  }
