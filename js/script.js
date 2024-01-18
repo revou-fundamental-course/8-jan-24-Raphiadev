e004cd478a03f5be9d7132533e64d68a1ca442a6
Raphiadev <raviachmadfadila@gmail.com>
2024-01-18 08:07:19 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function luas(){
+    var s1 = document.getElementById('s1').value;
+    var s2 = document.getElementById('s1').value;
+    var s3 = document.getElementById('s1').value;
+   
+    var luas =s2*s3;
+
+   document.getElementById('luas').innerHTML = luas;
+}
